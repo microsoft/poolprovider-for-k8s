97a01104ef981eea0a9681a39a650779dfe4073e
Divyansh Manchanda <divman@microsoft.com>
2019-06-11 15:43:41 +0530

    v1.10 unmarshal

--- kubernetes.go
+++ kubernetes.go
@@ -20,52 +20,52 @@ func CreatePod() string {
 	}
 
 	var podYaml = `
-apiVersion: v1
-kind: Pod
-metadata:
-   name: vsts-agent-dind
-spec:
-  containers:
-  - name: vsts-agent
-    image: microsoft/vsts-agent:ubuntu-16.04-docker-18.06.1-ce-standard
-    env:
-    - name: VSTS_ACCOUNT
-      valueFrom:
-        secretKeyRef:
-          name: vsts
-          key: VSTS_ACCOUNT
-    - name: VSTS_TOKEN
-      valueFrom:
-        secretKeyRef:
-          name: vsts
-          key: VSTS_TOKEN
-    - name: VSTS_POOL
-      value: divman
-    - name: DOCKER_HOST
-      value: tcp://localhost:2375
-  - name: dind-daemon
-    image: docker:18.09.6-dind
-    securityContext:
-      privileged: true
-    volumeMounts:
-    - name: daemon-storage
-      mountPath: /var/lib/docker
-  volumes:
-  - name: daemon-storage
-    emptyDir: {}
-	`
+    apiVersion: v1
+    kind: Pod
+    metadata:
+       name: vsts-agent-dind
+    spec:
+      containers:
+      - name: vsts-agent
+        image: microsoft/vsts-agent:ubuntu-16.04-docker-18.06.1-ce-standard
+        env:
+        - name: VSTS_ACCOUNT
+          valueFrom:
+            secretKeyRef:
+              name: vsts
+              key: VSTS_ACCOUNT
+        - name: VSTS_TOKEN
+          valueFrom:
+            secretKeyRef:
+              name: vsts
+              key: VSTS_TOKEN
+        - name: VSTS_POOL
+          value: divman
+        - name: DOCKER_HOST
+          value: tcp://localhost:2375
+      - name: dind-daemon
+        image: docker:18.09.6-dind
+        securityContext:
+          privileged: true
+        volumeMounts:
+        - name: daemon-storage
+          mountPath: /var/lib/docker
+      volumes:
+      - name: daemon-storage
+        emptyDir: {}
+        `
 
 	var p1 v1.Pod
 	err1 := yaml.Unmarshal([]byte(podYaml), &p1)
 	if err1 != nil {
-		return err1.Error()
+		return "unmarshal error: " + err1.Error()
 	}
 
 	// running the app in the default namespace. Pass namespace to pods method.
 	podClient := clientset.CoreV1().Pods("")
 	pod, err2 := podClient.Create(&p1)
 	if err2 != nil {
-		return err2.Error()
+		return "podclient create error: " + err2.Error()
 	}
 
 	return pod.GetName()
