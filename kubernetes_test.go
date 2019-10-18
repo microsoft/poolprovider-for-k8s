00b64f171cae9eb20cfa3eb6aa427c7c879dc2b1
Preeti Bansal <prebansa@microsoft.com>
2019-10-18 15:14:52 +0530

    build trigger1

--- kubernetes_test.go
+++ kubernetes_test.go
@@ -9,11 +9,12 @@ import (
 func TestCreatePod(t *testing.T) {
 	var agentrequest AgentRequest
 	agentrequest.AgentId = "1"
+
 	os.Setenv("TESTING","True")
 
 	//clientset := fake.NewSimpleClientset()
 	pod := CreatePod(agentrequest)
-	
+
 	//secret := createSecret(&clientset,agentrequest)
 	if (pod.Accepted == true){
 		fmt.Println("Pod created", pod)
